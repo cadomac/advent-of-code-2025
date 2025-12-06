9690a38e3d602000f353e7719bb6be1d65b8518c
Cameron MacDonald <12107773+cadomac@users.noreply.github.com>
2025-12-06 00:12:18 -0600

    feat: day 1 part 2 complete

--- day1/day1.py
+++ day1/day1.py
@@ -1,23 +1,54 @@
 import math
 
-current_pos = 50
-count = 0
+inputData = [i.strip() for i in open('./input.txt', 'r')]
+testData = [i.strip() for i in open('./test-input.txt', 'r')]
 
-def rotate(distance, direction):
-    subtotal = int(current_pos + distance)
-    if direction == 'L':
-        subtotal = int(current_pos - distance)
-    return subtotal % 100
+def part1():
 
-with open('./input.txt', 'r') as file:
-    while line := file.readline():
-        line = line.strip()
-        direction = line[0]
-        distance = int(line[1:])
-        print(f"Current position: {current_pos}, {line}")
+    current_pos = 50
+    count = 0
+
+    def rotate(distance, direction):
+        subtotal = int(current_pos + distance)
+        if direction == 'L':
+            subtotal = int(current_pos - distance)
+        return subtotal % 100
+
+    for input in inputData:
+        direction = input[0]
+        distance = int(input[1:])
         current_pos = rotate(distance, direction)
-        print(f"New position: {current_pos}")
         if current_pos == 0:
             count += 1
 
-print(f"Final position: {current_pos}, Password: {count}")
+    print(f"\nPart 1 | Password: {count}\n")
+
+def part2():
+    current_pos = 50
+    count = 0
+
+    def rotate(distance, direction):
+        times_passed_zero = 0
+        passes, rotation = divmod(distance, 100)
+        times_passed_zero += passes
+
+        if current_pos + rotation >= 100 and direction == 'R':
+            times_passed_zero += 1
+        if current_pos and current_pos - rotation <= 0 and direction == 'L':
+            times_passed_zero += 1
+        new_pos = int(current_pos + rotation) if direction == 'R' else int(current_pos - rotation)
+        new_pos %= 100
+        
+        return [new_pos, times_passed_zero]
+            
+
+    for input in inputData:
+        direction = input[0]
+        distance = int(input[1:])
+        current_pos, times_passed_zero = rotate(distance, direction)
+        count += times_passed_zero
+
+    print(f"Part 2 | Password: {count}\n")
+
+part1()
+part2()
